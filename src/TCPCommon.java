f8f2d4b5347b87583dec85971e825a6c90d7d5c3
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-10-29 13:45:51 +0100

    Add public timeout value to TCPCommon

--- src/TCPCommon.java
+++ src/TCPCommon.java
@@ -12,6 +12,7 @@ import java.net.Socket;
 public class TCPCommon {
 	// Size (Bytes) of the packets to send
 	public static final int BUF_SIZE = 4096;
+	public static final int TIMEOUT = 5000;
 	
 	/**
 	 * Return a client socket already connected to a server for direct use
