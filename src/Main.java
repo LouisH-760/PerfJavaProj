74ab3519096319f45ea4fa239d278e1b74b20217
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-10-30 17:04:41 +0100

    Continue implementation and modify pom.xml to have launchable jar files

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,14 @@
-import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) {
-		Thread t = new Thread(new StationLogic(25642));
+		Thread t;
 		try {
+			t = new Thread(new StationLogic(25642));
+			t.run();
 			Thread t1 = new Thread(new SensorLogic("aa", "bb", 25642));
 			t1.run();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		t.run();
-		try {
 			t.join();
-		} catch (InterruptedException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
--- src/StationLogic.java
+++ src/StationLogic.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 
 public class StationLogic implements Runnable{
 
@@ -9,15 +10,28 @@ public class StationLogic implements Runnable{
 	
 	private Message draft;
 	private Thread t_sender;
+	public volatile boolean cont;
 	
-	public StationLogic(int port) {
+	public StationLogic(int port) throws IOException {
 		this.port = port;
+		receiver = new TCPReceiver(port);
+		t_receiver = new Thread(receiver);
+		cont = true;
 	}
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		t_receiver.run();
+		draft = new Message(Message.TYPE_DATA, "aa", "1000");
+		t_sender = new Thread(new TCPThrowawaySender("127.0.0.1", port, draft));
+		t_sender.run();
+		while(cont) {
+			if(!receiver.haystack.isEmpty()) {
+				tmp_msg = receiver.haystack.remove(0);
+				System.out.println(tmp_msg);
+			}
+		}
+		receiver.cont = false;
 	}
 
 }
