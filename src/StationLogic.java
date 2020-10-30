5ca4f9555f931e423996f1cb340821a463aef0bf
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-10-30 20:15:15 +0100

    Start actual work on the station

--- src/StationLogic.java
+++ src/StationLogic.java
@@ -22,26 +22,6 @@ public class StationLogic implements Runnable{
 	@Override
 	public void run() {
 		t_receiver.start();
-		draft = new Message(Message.TYPE_DATA, "127.0.0.1", "1000");
-		t_sender = new Thread(new TCPThrowawaySender("127.0.0.1", TCPCommon.SENSOR_PORT, draft));
-		t_sender.start();
-		while(cont) {
-			if(!receiver.haystack.isEmpty()) {
-				Iterator<ReceivedMessage> bob = receiver.haystack.iterator();
-				while(bob.hasNext()) {
-					tmp_msg = bob.next();
-					bob.remove();
-					System.out.println(tmp_msg.getContents().split(Message.WEAK_SEP)[1]);
-				}
-					
-			}
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
 		receiver.cont = false;
 	}
 
