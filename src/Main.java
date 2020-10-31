fa87242e8ea6ea55f2e83bd1926b7cef468b2d0c
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-10-31 15:24:05 +0100

    Overhaul main logic and Station display

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) throws IOException, InterruptedException{
 		Thread t;
-		int port = 10000;
 		Scanner input = new Scanner(System.in);
-		System.out.println("a for station");
+		System.out.println("Type in 's' to start this as a station, any other input will start as a sensor.");
+		System.out.println("If starting as a sensor, input at least one value before requesting data from the station to avoid default values");
+		System.out.print(">");
 		String tmp = input.nextLine();
-		if(tmp.equals("a")) {
+		if(tmp.equals("s")) {
 			t = new Thread(new StationLogic());
 		} else {
 			t = new Thread(new SensorLogic("aa", "bb"));
 		}
 		t.start();
 		t.join();
+		input.close();
 	}
 }
--- src/StationQuickDisplay.java
+++ src/StationQuickDisplay.java
@@ -1,7 +1,7 @@
 
 public class StationQuickDisplay {
 	public static void minmax(double min, double max) {
-		System.out.println(String.format("Lowest: %.02s, Highest %.02s", min, max));
+		System.out.println(String.format("Lowest: %.05s, Highest %.05s", min, max));
 	}
 	
 	public static void notEnoughValues() {
